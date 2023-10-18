b8bbcf0c9df4d528c430d89920a95fe9d45122f5
btims512 <benjammintims@gmail.com>
2023-10-18 12:16:26 -0600

    style update & password input

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -3,10 +3,15 @@ import axios from "axios";
 const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
-    const { value } = e.target[0];
+    const usernameValue = e.target.username.value;
+    const passwordValue = e.target.password.value;
+
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
-      .then((r) => props.onAuth({ ...r.data, secret: value }))
+      .post("http://localhost:3001/authenticate", {
+        username: usernameValue,
+        password: passwordValue,
+      })
+      .then((r) => props.onAuth({ ...r.data, secret: passwordValue }))
       .catch((e) => console.log("error", e));
   };
 
@@ -14,12 +19,21 @@ const AuthPage = (props) => {
     <div className="background">
       <form onSubmit={onSubmit} className="form-card">
         <div className="form-title">Welcome 👋</div>
-
-        <div className="form-subtitle">Set a username to get started</div>
-
+        <div className="form-subtitle">Sign in to get started</div>
         <div className="auth">
-          <div className="auth-label">Username</div>
-          <input className="auth-input" name="username" />
+          <div className="auth-section">
+            <div className="auth-label">Username</div>
+            <input className="auth-input" name="username" required />
+          </div>
+          <div className="auth-section">
+            <div className="auth-label">Password</div>
+            <input
+              className="auth-input"
+              name="password"
+              type="password"
+              required
+            />
+          </div>
           <button className="auth-button" type="submit">
             Enter
           </button>
@@ -30,3 +44,4 @@ const AuthPage = (props) => {
 };
 
 export default AuthPage;
+
